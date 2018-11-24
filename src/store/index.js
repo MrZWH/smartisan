937f2187bb605845b4170522a7d3404a64e9ef77
MrZWH <1875290692@qq.com>
2018-11-24 18:02:08 +0800

    购物清单增删多选

--- src/store/index.js
+++ src/store/index.js
@@ -30,6 +30,35 @@ let store = new Vuex.Store({
         price += goods.price * goods.count
       })
       return price
+    },
+    allChecked(state) {
+      let allChecked = true
+      state.carPanelData.forEach(goods => {
+        if (!goods.checked) {
+          allChecked = false
+          return
+        }
+      })
+
+      return allChecked
+    },
+    checkedCount(state) {
+      let count = 0
+      state.carPanelData.forEach(goods => {
+        if (goods.checked) {
+          count += goods.count
+        }
+      })
+      return count
+    },
+    checkedPrice(state) {
+      let price = 0
+      state.carPanelData.forEach(goods => {
+        if (goods.checked) {
+          price += goods.count * goods.price
+        }
+      })
+      return price
     }
   },
   mutations: {
@@ -54,6 +83,7 @@ let store = new Vuex.Store({
         if (bOff) {
           let goodsData = data.info
           Vue.set(goodsData, 'count', data.count)
+          Vue.set(goodsData, 'checked', true)
           state.carPanelData.push(goodsData)
           state.carShow = true
           state.ball.show = true
@@ -69,6 +99,50 @@ let store = new Vuex.Store({
           return
         }
       })
+    },
+    plusCarPanelData(state, id) {
+      state.carPanelData.forEach((goods, index) => {
+        if (goods.sku_id === id) {
+          if (goods.count >= goods.limit_num) return
+          goods.count++
+          return
+        }
+      })
+    },
+    subCarPanelData(state, id) {
+      state.carPanelData.forEach((goods, index) => {
+        if (goods.sku_id === id) {
+          if (goods.count <= 1) return
+          goods.count--
+          return
+        }
+      })
+    },
+    checkGoods(state, id) {
+      state.carPanelData.forEach((goods, index) => {
+        if (goods.sku_id === id) {
+          goods.checked = !goods.checked
+          return
+        }
+      })
+    },
+    allCheckGoods(state, allChecked) {
+      state.carPanelData.forEach((goods, index) => {
+        goods.checked = !allChecked
+      })
+    },
+    delCheckGoods(state) {
+      let i = state.carPanelData.length
+      while (i--) {
+        if (state.carPanelData[i].checked) {
+          state.carPanelData.splice(i, 1)
+        }
+      }
+      // state.carPanelData.forEach((goods, index) => {
+      //   if (goods.checked) {
+      //     state.carPanelData.splice(index, 1)
+      //   }
+      // })
     },
     closePrompt(state) {
       state.maxCount = false
