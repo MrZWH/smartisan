abf574235a523a221b72a64495a0f06045453111
MrZWH <1875290692@qq.com>
2018-11-25 18:33:03 +0800

    我的订单页+我的收货地址

--- src/router/index.js
+++ src/router/index.js
@@ -7,11 +7,14 @@ import Item from "@/views/item";
 import Cart from "@/views/cart";
 import Checkout from "@/views/checkout";
 import Payment from "@/views/payment";
+import Account from "@/views/account";
+import Order from "@/views/account/order";
+import Address from '@/views/account/address'
 
 Vue.use(Router)
 
 export default new Router({
-  mode: history,
+  mode: 'history',
   routes: [
     {
       path: '/',
@@ -37,6 +40,20 @@ export default new Router({
       path: '/payment',
       name: 'Payment',
       component: Payment
+    },
+    {
+      path: '/account',
+      component: Account,
+      children: [{
+        path: '',
+        name: 'Account',
+        component: Order
+      },
+      {
+        path: '/address',
+        name: 'Address',
+        component: Address
+      }]
     }
   ]
 })
--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,5 @@
-import Vue from "vue";
-import Vuex from "vuex";
-import { stat } from "fs";
+import Vue from 'vue'
+import Vuex from 'Vuex'
 
 Vue.use(Vuex)
 
@@ -8,35 +7,38 @@ let store = new Vuex.Store({
   state: {
     carPanelData: [],
     receiveInfo: [{
-      "name": "王某某",
-      "phone": "13811111111",
-      "areaCode": "010",
-      "landLine": "64627856",
-      "provinceId": "110000",
-      "province": "北京市",
-      "cityId": "王某某",
-      "city": "市辖区",
-      "countyId": "110106",
-      "county": "海淀区",
-      "add": "乾隆路123号",
-      "default": true,
+      'name': '王某某',
+      'phone': '13811111111',
+      'areaCode': '010',
+      'landLine': '64627856',
+      'provinceId': 110000,
+      'province': '北京市',
+      'cityId': 110100,
+      'city': '市辖区',
+      'countyId': 110106,
+      'county': '海淀区',
+      'add': '上地十街辉煌国际西6号楼319室',
+      'default': true,
+      'checked': true
     },{
-      "name": "李某某",
-      "phone": "13811111111",
-      "areaCode": "010",
-      "landLine": "64627856",
-      "provinceId": "110000",
-      "province": "北京市",
-      "cityId": "王某某",
-      "city": "市辖区",
-      "countyId": "110106",
-      "county": "海淀区",
-      "add": "乾隆路123号",
-      "default": false,
+      'name': '李某某',
+      'phone': '13811111111',
+      'areaCode': '010',
+      'landLine': '64627856',
+      'provinceId': 110000,
+      'province': '北京市',
+      'cityId': 110100,
+      'city': '市辖区',
+      'countyId': 110106,
+      'county': '海淀区',
+      'add': '上地十街辉煌国际东6号楼350室',
+      'default': false,
+      'checked': false
     }],
+    provisionalOrder: [],
+    orderData: [],
     maxOff: false,
     carShow: false,
-    carTimer: null,
     ball: {
       show: false,
       el: null,
@@ -44,170 +46,205 @@ let store = new Vuex.Store({
     }
   },
   getters: {
-    totleCount(state) {
+    totleCount (state) {
       let count = 0
-      state.carPanelData.forEach(goods => {
-        count += goods.count
+      state.carPanelData.forEach((goods) => {
+        count += goods.count;
+      })
+  	  return count
+  	},
+  	totlePrice (state) {
+  	  let total = 0
+  	  state.carPanelData.forEach((goods) => {
+  	    total += goods.price * goods.count
+  	  })
+      return total
+    },
+    checkedCount (state) {
+      let count = 0
+      state.carPanelData.forEach((goods) => {
+        if(goods.checked)
+        count += goods.count;
       })
       return count
     },
-    totlePrice(state) {
-      let price = 0
-      state.carPanelData.forEach(goods => {
-        price += goods.price * goods.count
+    checkedPrice (state) {
+      let total = 0
+      state.carPanelData.forEach((goods) => {
+        if(goods.checked)
+        total += goods.price * goods.count
       })
-      return price
+      return total
     },
-    allChecked(state) {
+    checkedCarPanelData (state) {
+      let checkedCarPanelData = []
+      state.carPanelData.forEach((goods) => {
+        if(goods.checked)
+        checkedCarPanelData.push(goods);
+      })
+      return checkedCarPanelData
+    },
+    allChecked (state) {
       let allChecked = true
-      state.carPanelData.forEach(goods => {
-        if (!goods.checked) {
+      state.carPanelData.forEach((goods) => {
+        if(!goods.checked){
           allChecked = false
           return
         }
       })
-
       return allChecked
     },
-    checkedCount(state) {
-      let count = 0
-      state.carPanelData.forEach(goods => {
-        if (goods.checked) {
-          count += goods.count
+    maxCount (state) {
+      let maxOff = false
+      state.carPanelData.forEach((goods) => {
+        if(goods.count>goods.limit_num){
+          goods.count--
         }
       })
-      return count
-    },
-    checkedPrice(state) {
-      let price = 0
-      state.carPanelData.forEach(goods => {
-        if (goods.checked) {
-          price += goods.count * goods.price
-        }
-      })
-      return price
-    },
-    checkedGoods(state) {
-      let checkedGoods = []
-      state.carPanelData.forEach(goods => {
-        if (goods.checked) {
-          checkedGoods.push(goods)
-        }
-      })
-
-      return checkedGoods
+      return maxOff
     }
   },
   mutations: {
-    addCarPanelData(state, data) {
-      let bOff = true
-      if (!state.ball.show) {
-        state.carPanelData.forEach(goods => {
-          if (goods.sku_id === data.info.sku_id) {
-            goods.count += data.count
-            bOff = false
-            if(goods.count > goods.limit_num) {
-              goods.count -= data.count
-              state.maxCount = true
-              return
-            }
-            state.carShow = true
-            state.ball.show = true
-            state.ball.img = data.info.all_image
-            state.ball.el = event.path[0]
-          }
-        })
-        if (bOff) {
-          let goodsData = data.info
-          Vue.set(goodsData, 'count', data.count)
-          Vue.set(goodsData, 'checked', true)
-          state.carPanelData.push(goodsData)
-          state.carShow = true
-          state.ball.show = true
-          state.ball.img = data.info.all_image
+  	addCarPanelData (state,data) {
+  	  let bOff = true
+  	  state.carPanelData.forEach((goods) => {
+  	    if(goods.sku_id === data[0].sku_id){
+  	      goods.count += data[1]
+  	      if(goods.count>goods.limit_num){
+  	        goods.count -= data[1]
+  	        state.maxOff = true
+  	        bOff = false
+  	        return
+  	      }
           state.ball.el = event.path[0]
-        }
-      }
-    },
-    delCarPanelData(state, id) {
-      state.carPanelData.forEach((goods, index) => {
-        if (goods.sku_id === id) {
-          state.carPanelData.splice(index, 1)
+          state.ball.show = true
+          state.ball.img = data[0].ali_image
+  	      bOff = false
+  	      state.carShow = true
+  	    }
+  	  })
+  	  if(bOff){
+  	    let goodsData = data[0]
+  	    Vue.set(goodsData,'count',data[1])
+  	    Vue.set(goodsData,'checked',true)
+  	    state.carPanelData.push(goodsData)
+  	    state.carShow = true
+        state.ball.el = event.path[0]
+        state.ball.show = true
+        state.ball.img = data[0].ali_image
+  	  }
+  	},
+    plusCarPanelData (state,id) {
+      state.carPanelData.forEach((goods,index) => {
+        if(goods.sku_id === id){
+          if(goods.count === goods.limit_num) return
+          goods.count ++
           return
         }
       })
     },
-    plusCarPanelData(state, id) {
-      state.carPanelData.forEach((goods, index) => {
-        if (goods.sku_id === id) {
-          if (goods.count >= goods.limit_num) return
-          goods.count++
+  	subCarPanelData (state,id) {
+  	  state.carPanelData.forEach((goods,index) => {
+        if(goods.sku_id === id){
+          if(goods.count === 1) return
+          goods.count --
           return
         }
       })
-    },
-    subCarPanelData(state, id) {
-      state.carPanelData.forEach((goods, index) => {
-        if (goods.sku_id === id) {
-          if (goods.count <= 1) return
-          goods.count--
+  	},
+  	delCarPanelData (state,id) {
+  	  state.carPanelData.forEach((goods,index) => {
+        if(goods.sku_id === id){
+          state.carPanelData.splice(index,1)
           return
         }
       })
-    },
-    checkGoods(state, id) {
-      state.carPanelData.forEach((goods, index) => {
-        if (goods.sku_id === id) {
+  	},
+    checkGoods (state,id) {
+      state.carPanelData.forEach((goods,index) => {
+        if(goods.sku_id === id){
           goods.checked = !goods.checked
+        }
+      })
+    },
+    allGoodsCheck (state,checked) {
+      if(checked){
+        state.carPanelData.forEach((goods,index) => {
+          goods.checked = false
+        })
+      }else{
+        state.carPanelData.forEach((goods,index) => {
+          goods.checked = true
+        })
+      }
+    },
+    delCheckGoods (state) {
+      let i = state.carPanelData.length
+      while(i--){
+        if(state.carPanelData[i].checked){
+          state.carPanelData.splice(i,1)
+        }
+      }
+    },
+    submitOrder (state,data) {
+      let i = state.carPanelData.length
+      while(i--){
+        if(state.carPanelData[i].checked){
+          state.carPanelData.splice(i,1)
+        }
+      }
+      state.orderData.unshift(data)
+      state.receiveInfo.forEach((receive) => {
+        receive.checked = receive.default
+      })
+    },
+    payNow (state,id) {
+      state.orderData.forEach((order,index) => {
+        if(order.orderId === id){
+          order.isPay = true
           return
         }
       })
     },
-    allCheckGoods(state, allChecked) {
-      state.carPanelData.forEach((goods, index) => {
-        goods.checked = !allChecked
-      })
+    alertPrompt (state) {
+      state.maxOff = true
     },
-    delCheckGoods(state) {
-      let i = state.carPanelData.length
-      while (i--) {
-        if (state.carPanelData[i].checked) {
-          state.carPanelData.splice(i, 1)
-        }
-      }
-      // state.carPanelData.forEach((goods, index) => {
-      //   if (goods.checked) {
-      //     state.carPanelData.splice(index, 1)
-      //   }
-      // })
+    closePrompt (state) {
+      state.maxOff = false
     },
-    closePrompt(state) {
-      state.maxCount = false
-    },
-    showCar(state) {
-      clearTimeout(state.carTimer)
+    showCar (state) {
       state.carShow = true
     },
-    hideCar(state) {
-      state.carTimer = setTimeout(() => {
-        state.carShow = false
-      }, 500)
+    hideCar (state) {
+      state.carShow = false
     },
-    submitReceive(state, data) {
-      if (data.default) {
-        state.receiveInfo.forEach((receive) => {
+    checkDefault (state,data) {
+      state.receiveInfo.forEach((receive,index) => {
+        if (receive==data) {
+          receive.default = true
+//        state.receiveInfo.unshift(state.receiveInfo.splice(index,1)[0])
+        } else {
           receive.default = false
-        })
-      }
-      state.receiveInfo.push(data)
-    },
-    submitOrder(state, data) {
-      state.orderData.unshift(data)
-      let i = state.carPanelData.length
-      while(i--) {
-        if (state.carPanelData[i].checked) {
-          state.carPanelData.splice(i, 1)
         }
+      })
+    },
+    checkOut (state,data) {
+      state.provisionalOrder = data
+    },
+    submitReceive (state,data) {
+      if(data[0].default){
+        state.receiveInfo.forEach((receive,index) => {
+          receive.default = false
+          receive.checked = false
+        })
+//      state.receiveInfo.unshift(data)
+//      return
+      }
+      if(data[1]==null){
+        state.receiveInfo.push(data[0])
+      }else{
+        console.log(data[0])
+        state.receiveInfo[data[1]] = data[0]
       }
     }
   }
